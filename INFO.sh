c931182520f8ce6cac9054ac0de18840b5c21755
bb-qq <bb-qq@users.noreply.github.com>
2019-08-31 16:37:06 +0900

    Added synology configuration.

---
+++ INFO.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (c) 2000-2017 Synology Inc. All rights reserved.
+
+source /pkgscripts-ng/include/pkg_util.sh
+
+package="r8152"
+version="2.12.0-1"
+displayname="RTL8152/RTL8153 driver"
+maintainer="bb-qq"
+arch="$(pkg_get_platform)"
+description="Realtek RTL8152/RTL8153 Based USB Ethernet Adapters driver."
+[ "$(caller)" != "0 NULL" ] && return 0
+pkg_dump_info
