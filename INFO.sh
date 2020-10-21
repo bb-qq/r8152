9f617e07daea8d9090d90d2ba9ab39f1d8397883
Sam-PUMP <8461788+Sam-PUMP@users.noreply.github.com>
2020-10-20 22:31:21 -0400

    Update INFO.sh

--- INFO.sh
+++ INFO.sh
@@ -4,10 +4,10 @@
 source /pkgscripts-ng/include/pkg_util.sh
 
 package="r8152"
-version="2.13.0-1"
-displayname="RTL8152/RTL8153 driver"
+version="2.14.0-1"
+displayname="RTL8152/8153/8156 driver"
 maintainer="bb-qq"
 arch="$(pkg_get_platform)"
-description="Realtek RTL8152/RTL8153 Based USB Ethernet Adapters driver."
+description="Realtek RTL8152/RTL8153/RTL8156 Based USB Ethernet Adapters driver."
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
