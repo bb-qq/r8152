f9d2442200d7c0c5f8cc559c220cbb64d065d28c
bb-qq <bb-qq@users.noreply.github.com>
2021-07-10 18:08:50 +0900

    Added DSM 7 support.

--- INFO.sh
+++ INFO.sh
@@ -8,6 +8,32 @@ version="2.15.0-1"
 displayname="RTL8152/RTL8153 driver"
 maintainer="bb-qq"
 arch="$(pkg_get_platform)"
-description="Realtek RTL8152/RTL8153 Based USB Ethernet Adapters driver."
+install_type="package"
+thirdparty="yes"
+
 [ "$(caller)" != "0 NULL" ] && return 0
+
+if [ "${PRODUCT_VERSION}" = "7.0" ]; then
+    os_min_ver="7.0-40000"
+    RUN_AS="package"
+    INSTRUCTION=' [DSM7 note] If this is the first time you are installing this driver, special steps are required. See the readme for details.'
+else
+    RUN_AS="root"
+fi
+
+cat <<EOS > `dirname $0`/conf/privilege
+{
+    "defaults": {
+        "run-as": "${RUN_AS}"
+    }
+}
+EOS
+
+cat <<EOS > `dirname $0`/SynoBuildConf/depends
+[default]
+all="${PRODUCT_VERSION}"
+EOS
+
+description="Realtek RTL8152/RTL8153 Based USB Ethernet Adapters driver.${INSTRUCTION}"
+
 pkg_dump_info
