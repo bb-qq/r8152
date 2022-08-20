d268ca82593be00a7b214eaef000ca65dddd4a63
bb-qq <bb-qq@users.noreply.github.com>
2022-08-20 16:23:00 +0900

    Bump version to 2.16.3-1.

--- INFO.sh
+++ INFO.sh
@@ -4,7 +4,7 @@
 source /pkgscripts-ng/include/pkg_util.sh
 
 package="r8152"
-version="2.15.0-10"
+version="2.16.3-1"
 displayname="RTL8152/RTL8153 driver"
 maintainer="bb-qq"
 arch="$(pkg_get_platform)"
