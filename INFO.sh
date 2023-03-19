4845130f7eb8b5aa59c52a62b41ceb48cc1a3b7a
bb-qq <bb-qq@users.noreply.github.com>
2023-03-19 11:31:10 +0900

    Supported DSM Toolkit 7.1 and above.

--- INFO.sh
+++ INFO.sh
@@ -13,7 +13,8 @@ thirdparty="yes"
 
 [ "$(caller)" != "0 NULL" ] && return 0
 
-if [ "${PRODUCT_VERSION}" = "7.0" ]; then
+PRODUCT_VERSION_WITHOUT_MICRO=`echo ${PRODUCT_VERSION} | sed -E 's/^([0-9]+\.[0-9]+).+$/\1/'`
+if [ 1 -eq $(echo "${PRODUCT_VERSION_WITHOUT_MICRO} >= 7.0" | bc) ]; then
     os_min_ver="7.0-40000"
     RUN_AS="package"
     INSTRUCTION=' [DSM7 note] If this is the first time you are installing this driver, special steps are required. See the readme for details.'
