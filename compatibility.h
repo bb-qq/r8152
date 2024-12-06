99bd66498450ec81ecd9cd67ae785d603ae12e77
bb-qq <bb-qq@users.noreply.github.com>
2024-12-07 00:46:56 +0900

    Added r8152.53.56-2.19.2.tar.bz2.

--- compatibility.h
+++ compatibility.h
@@ -21,6 +21,8 @@
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(3,7,0) */
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,31) */
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6,9,0)
+	#define ethtool_keee ethtool_eee
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0)
 	#define TSO_LEGACY_MAX_SIZE		65536
 	#define netif_napi_add_weight		netif_napi_add
@@ -637,6 +639,7 @@
 	}
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5,17,0) */
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0) */
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6,9,0) */
 
 #ifndef FALSE
 	#define TRUE	1
